5ccc0b2d7f0a8221ac65a92c7375b39ed337a4d7
victor.onyia.o@gmail.com <Vectorjay@github.com>
2023-12-01 22:56:51 +0100

    update file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import threading
-import signal
-import time
+# import threading
+# import signal
+# import time
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,29 +12,29 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# Function to be executed when a signal is received
-def signal_handler(sig, frame):
-    print(f"Received signal {sig}")
+# # Function to be executed when a signal is received
+# def signal_handler(sig, frame):
+#     print(f"Received signal {sig}")
 
-# Register signal handlers in the main thread
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+# # Register signal handlers in the main thread
+# signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGTERM, signal_handler)
 
-# Function to be run in a separate thread
-def thread_function():
-    while True:
-        time.sleep(1)
+# # Function to be run in a separate thread
+# def thread_function():
+#     while True:
+#         time.sleep(1)
 
-# Create and start the thread
-worker_thread = threading.Thread(target=thread_function)
-worker_thread.start()
+# # Create and start the thread
+# worker_thread = threading.Thread(target=thread_function)
+# worker_thread.start()
 
-# Keep the main thread alive
-while True:
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        break
+# # Keep the main thread alive
+# while True:
+#     try:
+#         time.sleep(1)
+#     except KeyboardInterrupt:
+#         break
 
-# Wait for the worker thread to finish
-worker_thread.join()
+# # Wait for the worker thread to finish
+# worker_thread.join()
