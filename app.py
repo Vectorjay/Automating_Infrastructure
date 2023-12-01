205ba98095d29b3c83c6d2a29e2d15e08d8fc3c8
victor.onyia.o@gmail.com <Vectorjay@github.com>
2023-12-01 22:42:41 +0100

    update file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import threading
-import queue
+import signal
 import time
 from flask import Flask, render_template
 
@@ -12,28 +12,29 @@ def index():
 if __name__ == '__main__':
     socketio.run(debug=True)
 
-# Create a queue instance for communication between threads
-message_queue = queue.Queue()
+# Function to be executed when a signal is received
+def signal_handler(sig, frame):
+    print(f"Received signal {sig}")
+
+# Register signal handlers in the main thread
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
 # Function to be run in a separate thread
 def thread_function():
     while True:
-        # Check if there's a message in the queue
-        if not message_queue.empty():
-            message = message_queue.get()
-            print(f"Received message: {message}")
         time.sleep(1)
 
 # Create and start the thread
 worker_thread = threading.Thread(target=thread_function)
 worker_thread.start()
 
-# Main thread sending messages to the worker thread
-for i in range(5):
-    message = f"Message {i+1}"
-    message_queue.put(message)
-    print(f"Sent: {message}")
-    time.sleep(2)
+# Keep the main thread alive
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
 
 # Wait for the worker thread to finish
 worker_thread.join()
