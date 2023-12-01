8442490fae9172817a8508350c8c3342f04ab8ba
victor.onyia.o@gmail.com <Vectorjay@github.com>
2023-12-01 20:39:55 +0100

    updated app file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+socketio = Flask(__name__)
 
-@app.route('/')
+@socketio.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(debug=False)
