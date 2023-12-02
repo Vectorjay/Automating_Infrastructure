ba265dd912156d2f711cc5a6e635fe9cf3b5ec2b
victor.onyia.o@gmail.com <Vectorjay@github.com>
2023-12-02 10:33:38 +0100

    update app file

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
+# from flask_socketio import SocketIO, emit
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#  SocketIO.run(app, debug=True)
+
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('message')
+def handle_message(message):
+    print('Received message: ' + message)
+    # Handle the message received from the client
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
